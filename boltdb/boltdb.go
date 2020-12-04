491ce92272646483f02c3d61be7d37c1f236cdd5
qiaoyu.peng <qiaoyu.peng@hex-tech.net>
2020-12-04 16:47:58 +0800

    feat: add boltdb.go

---
+++ boltdb/boltdb.go
@@ -0,0 +1,32 @@
+package boltdb
+
+import (
+	bolt "go.etcd.io/bbolt"
+	"log"
+)
+
+var db *bolt.DB
+
+func Open(path string) {
+	var err error
+	db, err = bolt.Open(path, 0666, nil)
+	if err != nil {
+		log.Println("Cannot connect db, path: ", path)
+	}
+	log.Println("connected!")
+}
+
+func Keys(bucket string) ([]string, error) {
+	var keys []string
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		c := b.Cursor()
+
+		for k, _ := c.First(); k != nil; k, _ = c.Next() {
+			keys = append(keys, string(k))
+		}
+		return nil
+
+	})
+	return keys, err
+}
