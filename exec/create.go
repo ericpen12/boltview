5e2da2e9be64cefe03cda4fae0d6cc4f1e7c5a70
qunzhen12 <quzhen12@gmail.com>
2020-12-23 09:46:59 +0800

    add command: create

---
+++ exec/create.go
@@ -0,0 +1,55 @@
+package exec
+
+import (
+	"boltview/boltdb"
+	"fmt"
+)
+
+type create struct {
+	base
+	bucketNames []string
+}
+
+func (c create) Description() string {
+	return c.description
+}
+
+func (c create) Parse(args []string) error {
+	if len(c.options) <= 1 {
+		return nil
+	}
+	c.options = args
+	c.bucketNames = c.options[1:]
+	return nil
+}
+
+func (c create) Exec() error {
+	for _, name := range c.bucketNames {
+		err := boltdb.CreateBucket(name)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c create) Error(err error) {
+	fmt.Println(err)
+}
+
+func (c create) Ok() {
+	printOK()
+}
+
+func (c create) CommandName() string {
+	return c.name
+}
+
+func NewCmdCreate() *create {
+	return &create{base{
+		name:        cmdCreate,
+		cmd:         cmdCreate,
+		description: descriptionCreate,
+		options:     nil,
+	}, nil}
+}
