8324eb10e5ed7ab843a6d1caf2a7f4e90125734a
qunzhen12 <quzhen12@gmail.com>
2020-12-27 10:07:17 +0800

    add command: exit

---
+++ exec/exit.go
@@ -0,0 +1,32 @@
+package exec
+
+import "os"
+
+const (
+	cmdExit         = "exit"
+	descriptionExit = "exit boltview"
+)
+
+type exit struct {
+	base
+}
+
+func init() {
+	register(newExit())
+}
+
+func newExit() *exit {
+	return &exit{
+		base{
+			name:        cmdExit,
+			cmd:         cmdExit,
+			description: descriptionExit,
+		},
+	}
+}
+
+func (e *exit) exec() error {
+	writeToConsole("bye.\n")
+	os.Exit(0)
+	return nil
+}
