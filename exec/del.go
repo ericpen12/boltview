67bcf61481aa3be4dd7909faa644f889d06bd5b4
qunzhen12 <quzhen12@gmail.com>
2020-12-27 10:38:49 +0800

    update command:delete, it can delete keys

--- exec/del.go
+++ exec/del.go
@@ -1,6 +1,9 @@
 package exec
 
-import "boltview/boltdb"
+import (
+	"boltview/boltdb"
+	"strings"
+)
 
 const (
 	cmdDel         = "del"
@@ -8,7 +11,7 @@ const (
 )
 
 type del struct {
-	create
+	base
 }
 
 func init() {
@@ -17,19 +20,34 @@ func init() {
 
 func newDel() *del {
 	return &del{
-		create{
-			base: base{
-				name:        cmdDel,
-				cmd:         cmdDel,
-				description: descriptionDel,
-			}}}
+		base: base{
+			name:        cmdDel,
+			cmd:         cmdDel,
+			description: descriptionDel,
+		}}
+}
+
+func (d *del) parse(args []string) error {
+	if len(args) < 2 {
+		return ErrInvalidParams
+	}
+	d.params = args
+	return nil
 }
 
 func (d *del) exec() error {
-	for _, bucket := range d.bucketNames {
-		err := boltdb.DeleteBucket(bucket)
-		if err != nil {
-			return err
+	for _, p := range d.params[1:] {
+		if strings.Contains(p, ".") {
+			args := strings.Split(p, ".")
+			err := boltdb.DeleteKey(args[0], args[1])
+			if err != nil {
+				return nil
+			}
+		} else {
+			err := boltdb.DeleteBucket(p)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
